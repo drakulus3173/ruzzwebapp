58994ff1c4ca9a1c04b8762f2a56f14ce1029141
drakulus3173 <drakulus3173@hotmail.com>
2016-04-20 15:19:21 +1200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="css/mystyles.css" rel="stylesheet" media="screen">
+
+  </head>
+  <body>
+    <h1>Hello Welcome to my page.</h1>
+    <h2><marquee> (/*_*)/</marquee></h2>
+    <h3><marquee direction="right" scrollamount="40"><img src="dog.gif"/></marquee></h3>
+    <h4><marquee width="60%" direction="up" behavior="alternate"><marquee direction="right" behavior="alternate" scrollamount="40">\(*,*)/</marquee></marquee></h4>
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  </body>
+</html>
